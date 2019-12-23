d9d245daa368c6ba5a230bbc05d30390ea6d9246
Aleksandr Demin <noidea91@gmail.com>
2019-12-23 22:04:25 +0300

    Update main.py

--- main.py
+++ main.py
@@ -103,6 +103,7 @@ def replaceDataInContent(content, region, service):
 
 # With subdomain
 
+@app.route('/')
 @app.route('/', subdomain="<subdomain>")
 def RegionNoService(subdomain):
     print('Subdomain ' + subdomain)
@@ -127,6 +128,7 @@ def RegionNoService(subdomain):
         regions = db.getRegionsTree(parentIds=[region['id']])
         )
 
+@app.route('/<path:routeString>')
 @app.route('/<path:routeString>', subdomain="<subdomain>")
 def RegionService(routeString, subdomain):
     print('Subdomain ' + subdomain)
